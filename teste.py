63cb8f3a6bbd5e767f080de6859fd98b779fdcae
francis <francis.vargas@rnp.br>
2026-06-02 19:55:19 -0300

    feat: adicionando função de multiplicação

--- teste.py
+++ teste.py
@@ -2,8 +2,12 @@ a = int(input("Digite um valor:"))
 
 b = int(input("Digite outro valor:"))
 #corrigido
+def multiplicar(valor1, valor2):
+    return valor1*valor2
 def somar(valor1, valor2):
           return valor1 + valor2
 
 soma = somar(a,b)
+mult = multiplicar(a,b)
+print(mult)
 print(soma)
